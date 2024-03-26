afdf089a4734ebaee282abfc601033c3ea3491bc
AnanduSP <92517928+AnanduSP@users.noreply.github.com>
2024-03-26 11:19:46 +0530

    identity-service project added

---
+++ identity-service/src/main/java/com/ford/demo/identityservice/IdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ford.demo.identityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApplication.class, args);
+	}
+
+}
